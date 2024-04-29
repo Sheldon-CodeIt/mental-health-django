6df45e50f96d981dd0399b1f769f5357a3c21d46
Sheldon <sheldonchettiar3@gmail.com>
2024-04-29 16:25:15 +0530

    added static files

--- baseApp/urls.py
+++ baseApp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.Home, name="home"),
+    # path('roadmap/', views.RoadmapPage, name='roadmap'),
+    # path('about/', views.AboutPage, name='about'),
+    # path('login/', views.LoginPage, name='login'),
+    # path('signup/', views.SignupPage, name='signup'),
+    # path('logout/',views.LogoutPage, name='logout'),
+    # path('profile/',views.ProfilePage, name='profile'),
 ]
