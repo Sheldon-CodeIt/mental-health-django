06536b95fb8c4dd51b4d25c97046221a0439f928
Sheldon <sheldonchettiar3@gmail.com>
2024-04-29 16:39:15 +0530

    staticfiles configuration

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,7 +109,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static') 
+]
+
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles', 'static') 
 ]
 
 # Default primary key field type
