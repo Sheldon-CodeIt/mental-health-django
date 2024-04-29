b1968584fb1122c847696d31b342b98310f33859
Sheldon <sheldonchettiar3@gmail.com>
2024-04-29 17:15:30 +0530

    static configuration at urls

--- mental_health/settings.py
+++ mental_health/settings.py
@@ -121,3 +121,4 @@ if os.environ.get("VERCEL"):
     # Turn on WhiteNoise storage backend that takes care of compressing staticfiles
     # and creating unique names for each version so they can safely be cached forever
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
